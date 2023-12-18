2e604ae4780d1e313433da8c17d6ac15d7419d22
makecraft <makeshiro@outlook.com>
2023-12-18 14:30:43 -0500

    act

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+import gi
+
+gi.require_version("Gtk", "4.0")
+from gi.repository import GLib, Gtk
+
+
+class MyApplication(Gtk.Application):
+    def __init__(self):
+        super().__init__(application_id="com.example.MyGtkApplication")
+        GLib.set_application_name('My Gtk Application')
+
+    def do_activate(self):
+        window = Gtk.ApplicationWindow(application=self, title="Hello World")
+        window.present()
+
+
+app = MyApplication()
+exit_status = app.run(sys.argv)
+sys.exit(exit_status)
